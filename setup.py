33f1dac1b8af4a670eb2ab76ff6b58dfad8d3175
mohammad <mohammadnajm75@gmail.com>
2025-05-17 14:55:19 +0330

    add memory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask_watchlog_apm',
-    version='0.1.0',
+    version='0.1.1',
     description='Lightweight APM for Flask that sends metrics to Watchlog Agent',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
